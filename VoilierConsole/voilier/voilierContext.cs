e2e175d6a0348a68fb6a6c423e43d992964665de
zmo2s <antoninleo1011@gmail.com>
2021-04-03 21:03:58 +0200

    tout_marche clear

---
+++ VoilierConsole/voilier/voilierContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ConsoleApp1.voilier
+{
+    public partial class voilierContext : DbContext
+    {
+        public voilierContext()
+        {
+        }
+
+        public voilierContext(DbContextOptions<voilierContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<EfmigrationsHistory> EfmigrationsHistory { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySQL("server=127.0.0.1;port=3306;user=phpmyadmin;password=Clementine1011%;database=voilier");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EfmigrationsHistory>(entity =>
+            {
+                entity.HasKey(e => e.MigrationId)
+                    .HasName("PRIMARY");
+
+                entity.ToTable("__EFMigrationsHistory");
+
+                entity.Property(e => e.MigrationId).HasMaxLength(150);
+
+                entity.Property(e => e.ProductVersion)
+                    .IsRequired()
+                    .HasMaxLength(32);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
